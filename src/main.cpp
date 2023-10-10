07711abbd82f0dc67663dee363815269e4fa1666
CoderKoen <hual84614@gmail.com>
2023-10-10 18:14:14 -0400

    Added ls/dir and a file system thing

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,53 @@
-#include <iostream>
-#include <string>
-#include <string.h>
-#include <vector>
+#include "../lib/util.hpp"
 
 std::string location = "C\\";
 
+//Mock files and folders
 std::vector<std::vector<std::string>> file_folders = {
-    {"None","C\\","Folder"}
+    {"None","C\\","Folder"},
+    {"C\\","Users","Folder"}
 };
 
-std::string command;
+std::string user_input;
+
+void run_command(std::string command){
+    if (command == "exit")
+    {
+        exit(0);
+    }
+    else if (command == "pwd")
+    {
+        std::cout << std::endl << "Path" << std::endl;
+        std::cout << "----" << std::endl;
+        std::cout << location << std::endl << std::endl;
+    }
+    else if (command == "ls" || command == "dir")
+    {
+        for (int i = 0; i < file_folders.size(); i++)
+        {
+            if (file_folders[i][0] == location)
+            {
+                std::cout << std::endl << "Items in " << location << std::endl;
+                std::cout << "--------" << std::endl;
+                std::cout << file_folders[i][1] << " ["<< file_folders[i][2] << "]" << std::endl;
+                
+            }
+            
+        }
+        std::cout << std::endl;
+    }
+    else
+    {
+        std::cout << command << " isn't a valid command!!!" << std::endl;
+    }
+}
+
 
 void run_shell()
 {
-    std::cout << "["<< location << "]|> ";
-    std::cin >> command;
-    //run_command(command)
+    std::cout << "["<< location << "]> ";
+    std::cin >> user_input;
+    run_command(user_input);
     run_shell();
 }
 
