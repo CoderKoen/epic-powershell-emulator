7d5e84ccc4545e0c2415e393b398c7b5dd9c7713
CoderKoen <hual84614@gmail.com>
2023-10-10 16:34:55 -0400

    Added run_shell()

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <string>
+#include <string.h>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, world!" << std::endl;
+std::string location = "C\\";
+
+std::vector<std::vector<std::string>> file_folders = {
+    {"None","C\\","Folder"}
+};
+
+std::string command;
+
+void run_shell()
+{
+    std::cout << "["<< location << "]|> ";
+    std::cin >> command;
+    //run_command(command)
+    run_shell();
+}
+
+
+
+int main(void) 
+{
+    run_shell();
     return 0;
 }
